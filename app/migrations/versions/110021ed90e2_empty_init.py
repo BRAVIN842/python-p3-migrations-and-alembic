cde14bd0ead9f48f0d484039754185a5ad94f808
Bravin <onsasebravin853@gmail.com>
2023-12-20 21:13:15 +0300

    Complete

---
+++ app/migrations/versions/110021ed90e2_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 110021ed90e2
+Revises: 
+Create Date: 2023-12-20 21:04:58.894368
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '110021ed90e2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
